a1630d444b0119462dfcd6f51b2c02a62360a606
Wanessa Barros Lino <108156873+wanessabarroslino@users.noreply.github.com>
2024-06-23 11:08:01 -0300

    Initial commit

---
+++ js/main.js
@@ -0,0 +1,28 @@
+
+function tocaSom(idElementoAudio) {
+    document.querySelector(idElementoAudio).play();
+}
+
+const listaDeTeclas = document.querySelectorAll('.tecla');
+
+for (let i = 0; i < listaDeTeclas.length; i++) {
+
+    const tecla = listaDeTeclas[i];
+    const instrumento = tecla.classList[1];
+    const idAudio = `#som_${instrumento}`
+
+    tecla.onclick = function (){
+        tocaSom(idAudio);
+    }
+
+    tecla.onkeydown = function(evento) {
+        if (evento.code == 'Space' || evento.code == 'Enter' ) {
+            tecla.classList.add('ativa');
+        }
+    }
+
+    tecla.onkeyup = function() {
+        tecla.classList.remove('ativa');
+    }
+    
+}
